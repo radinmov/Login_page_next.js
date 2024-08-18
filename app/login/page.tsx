e30add4745a620775aa1ac58c6124590a5e0bfc4
radinmov <radinmovahedi1@gmail.com>
2024-08-18 23:22:42 +0330

    1.0.2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,61 +1,32 @@
 'use client'
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import "./Style.css"
 
-
-
-export const metadata = {
-  title: 'Login page for behzad agadam farid'
-}
 export default function Login() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
 
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
 
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  useEffect(() => {
+    document.title = 'Login';
+  }, []);
 
-    console.log('Logging in with:', { email, password });
 
-    if (email === 'test@example.com' && password === 'password') {
-      alert('Login successful!');
-    } else {
-      alert('Invalid credentials');
-    }
-  };
 
   return (
-    <div className='bg-gray flex justify-center h-screen'>
-      <form onSubmit={handleSubmit} className='flex flex-col ' style={{width: '300px' }}>
-        <h2>Login</h2>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-          style={{ padding: '10px', marginBottom: '15px' }}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-          required
-          style={{ padding: '10px', marginBottom: '15px' }}
-        />
-        <button type="submit" style={{ padding: '10px', cursor: 'pointer' }}>
-          Login
-        </button>
-      </form>
+    <div className='container'>
+      <div className='container_inner'>
+        <div className='container-right'>
+          <h1 className='font-extrabold text-3xl'>Login page </h1>
+          <div className='ints'>
+            <input className='rounded-md' type='text' placeholder='Your name' />
+            <input className='rounded-md' type='text' placeholder='Your Email' />
+            <input className='rounded-md' type='password' placeholder='password' />
+          </div>
+          <button className='bg-blue-500'>Login</button>
+        </div>
+        <div className='container-left'>
+
+        </div>
+      </div>
     </div>
   );
 }
